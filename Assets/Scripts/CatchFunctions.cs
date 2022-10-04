70e589d51a24f760cfb529d016dbdea05330cbbe
Gocer615 <gtumayy21@gmail.com>
2022-10-04 14:55:31 +0300

    refactor: platform activity checked

--- Assets/Scripts/CatchFunctions.cs
+++ Assets/Scripts/CatchFunctions.cs
@@ -7,41 +7,57 @@ public class CatchFunctions : MonoBehaviour
     public GameObject leftPlatform;
     public GameObject midPlatform;
     public GameObject rightPlatform;
+    public bool activePlat = false;
     
     public void LeftPlat()
     {
-        leftPlatform.SetActive(true);
-        StartCoroutine(LeftReset());
+        if (activePlat == false)
+        {
+            activePlat = true;
+            leftPlatform.SetActive(true);
+            StartCoroutine(LeftReset());
+        }
     }
 
     public void MidPlat()
     {
-        midPlatform.SetActive(true);
-        StartCoroutine(MidReset());
+        if (activePlat == false)
+        {
+            activePlat = true;
+            midPlatform.SetActive(true);
+            StartCoroutine(MidReset());
+        }
     }
 
     public void RightPLat()
     {
-        rightPlatform.SetActive(true);
-        StartCoroutine(RightReset());
+        if (activePlat == false)
+        {
+            activePlat = true;
+            rightPlatform.SetActive(true);
+            StartCoroutine(RightReset());
+        }
     }
 
     IEnumerator LeftReset()
     {
         yield return new WaitForSeconds(0.5f);
         leftPlatform.SetActive(false);
+        activePlat = false;
     }
 
     IEnumerator RightReset()
     {
         yield return new WaitForSeconds(0.5f);
         rightPlatform.SetActive(false);
+        activePlat = false;
     }
 
     IEnumerator MidReset()
     {
         yield return new WaitForSeconds(0.5f);
         midPlatform.SetActive(false);
+        activePlat = false;
     }
 
 }
