38df9c2a14e3a216ceda551956e457fba07ba265
Gocer615 <gtumayy21@gmail.com>
2022-10-05 15:13:33 +0300

    refactor: time revized

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,13 +8,17 @@ public class Timer : MonoBehaviour
     public GameObject timeDisplay;
     public int currentSeconds = 30;
     public bool countingDown = false;
+    public bool isZero = false;
 
     void Update()
     {
-        if (countingDown == false)
+        if (isZero== false)
         {
-            countingDown = true;
-            StartCoroutine(SubtractSecond());
+            if (countingDown == false)
+            {
+                countingDown = true;
+                StartCoroutine(SubtractSecond());
+            }
         }
     }
 
@@ -22,6 +26,10 @@ public class Timer : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
         currentSeconds -= 1;
+        if (currentSeconds == 0)
+        {
+            isZero = true;
+        }
         timeDisplay.GetComponent<TextMeshProUGUI>().text = "Time " + currentSeconds;
         countingDown = false;
     }
